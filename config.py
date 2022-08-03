bd399ab1b626c797bfcad60751e026af851b4b66
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-08-03 20:01:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,13 @@ if os.path.exists("local.env"):
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "KK_Updates")
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH","")
-BOT_USERNAME = getenv("BOT_USERNAME")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "kk_kovilakam")
+BOT_USERNAME = getenv("BOT_USERNAME",")
+SUPPORT = getenv("SUPPORT", "kk_kovilakam")
+OWNER_ID = int(getenv("OWNER_ID", "5423253221"))
 
 
 
