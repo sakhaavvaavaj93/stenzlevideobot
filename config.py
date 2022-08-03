e6352cd0c83cfab08317fcdb17d72b1d3c4e4383
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-08-03 19:45:00 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+from os import path
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "KK_Updates")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH","")
+BOT_USERNAME = getenv("BOT_USERNAME")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "kk_kovilakam")
+
+
+
+
