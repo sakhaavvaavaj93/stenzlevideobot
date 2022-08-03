35edfe74a9a23ffd57a04987fe52ea5846bb4f6c
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-08-03 21:34:58 +0530

    Update stenzlevideobot.py

--- stenzlevideobot.py
+++ stenzlevideobot.py
@@ -87,7 +87,7 @@ HELP_BUTTON = InlineKeyboardMarkup(
 
 @bot.on_message(
 
-    commandpro(["/play", "/yt", "/ytp", "play", "yt", "ytp", "@", "#"])
+    commandpro(["/vplay", "vplay"])
 
     & filters.group
     & ~filters.edited
@@ -192,7 +192,7 @@ async def yt_audio(link):
     else:
         return 0, stderr.decode()
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
+@bot.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/d0b2899c25498fb52d0c8.jpg",
