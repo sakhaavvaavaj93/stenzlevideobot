8b00035e2ccd8ba1c7ac2cf576e2a8f5d1b84983
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-08-03 19:14:12 +0530

    Update queues.py

--- helpers/queues.py
+++ helpers/queues.py
@@ -2,7 +2,40 @@ from typing import Dict
 from asyncio import Queue, QueueEmpty as Empty
 
 
-queues: Dict[int, Queue] = {}
+QUEUE = {}
+
+def add_to_queue(chat_id, title, duration, ytlink, playlink, type, quality, thumb):
+    if chat_id in QUEUE:
+        chat_queue = QUEUE[chat_id]
+        chat_queue.append([title, duration, ytlink, playlink, type, quality, thumb])
+        return int(len(chat_queue) - 1)
+    else:
+        QUEUE[chat_id] = [[title, duration, ytlink, playlink, type, quality, thumb]]
+
+
+def get_queue(chat_id):
+    if chat_id in QUEUE:
+        chat_queue = QUEUE[chat_id]
+        return chat_queue
+    else:
+        return 0
+
+
+def pop_an_item(chat_id):
+    if chat_id in QUEUE:
+        chat_queue = QUEUE[chat_id]
+        chat_queue.pop(0)
+        return 1
+    else:
+        return 0
+
+
+def clear_queue(chat_id):
+    if chat_id in QUEUE:
+        QUEUE.pop(chat_id)
+        return 1
+    else:
+        return 0
 
 
 async def put(chat_id: int, **kwargs) -> int:
@@ -11,13 +44,6 @@ async def put(chat_id: int, **kwargs) -> int:
     await queues[chat_id].put({**kwargs})
     return queues[chat_id].qsize()
 
-
-def get(chat_id: int) -> Dict[str, str]:
-    if chat_id in queues:
-        try:
-            return queues[chat_id].get_nowait()
-        except Empty:
-            return None
 
 
 def is_empty(chat_id: int) -> bool:
@@ -32,12 +58,3 @@ def task_done(chat_id: int):
             queues[chat_id].task_done()
         except ValueError:
             pass
-
-
-def clear(chat_id: int):
-    if chat_id in queues:
-        if queues[chat_id].empty():
-            raise Empty
-        else:
-            queues[chat_id].queue = []
-    raise Empty
